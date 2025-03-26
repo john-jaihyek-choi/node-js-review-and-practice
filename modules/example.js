d53b5f4294db7d5136114cf1c7c1145331e17cde
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-03-26 18:06:35 -0500

    modules reviewed

---
+++ modules/example.js
@@ -0,0 +1,8 @@
+const exampleList1 = ["Mario", "Yoshi", "Peach"];
+const exampleList2 = ["Luigi", "Toad", "Bowser"];
+
+// module.exports to export objects from the current module to be imported in other modules.
+module.exports = {
+  list1: exampleList1,
+  list2: exampleList2,
+};
---
+++ modules/modules.js
@@ -0,0 +1,10 @@
+const { list1, list2 } = require("./example");
+
+console.log(list1);
+console.log(list2);
+
+// some standard modules available in node
+const os = require("os");
+
+console.log(os.platform);
+console.log(os.homedir());
