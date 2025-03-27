8b478b2eef2d40eeece9a6e9924e81647c3d2db4
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-03-26 23:02:17 -0500

    routing via ejs reviewed

--- express/app.js
+++ express/app.js
@@ -1,25 +1,47 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "./express/views");
+
 app.listen(3000, () => {
-    console.log("listening on 3000....")
-})
+  console.log("listening on 3000....");
+});
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../clientsAndServers/views/index.html"));
-})
+  // plain routing using sendFile
+  // res.sendFile(path.join(__dirname, "../clientsAndServers/views/index.html"));
+
+  // using ejs view
+  res.render("index", { val: "home content" });
+});
 
 app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "../clientsAndServers/views/about.html"));
-})
+  // plain routing using sendFile
+  // res.sendFile(path.join(__dirname, "../clientsAndServers/views/about.html"));
 
-app.get("/about-me", (req, res) =>  {
-    res.redirect("/about")
-})
+  // using ejs view
+  res.render("about", { val: "about content" });
+});
+
+// redirecting other endpoints
+app.get("/about-me", (req, res) => {
+  res.redirect("/about");
+});
+
+app.get("blog/create", (req, res) => {
+  res.render("create", { val: "blog content" });
+});
 
 // will only execute once code reaches this far
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "../clientsAndServers/views/404.html"));
-})
+  // plain routing using sendFile
+  //   res
+  //   .status(404)
+  //   .sendFile(path.join(__dirname, "../clientsAndServers/views/404.html"));
+
+  // using ejs view
+  res.status(404).render("404", { val: "blog content" });
+});
